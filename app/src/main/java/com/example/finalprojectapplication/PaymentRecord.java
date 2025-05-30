c60ba733ff374f55c0e7e7bb69e0feeb04651b99
JXR <jxr19870689720@163.com>
2025-05-30 17:08:02 +0800

    获取记录（户号，金额，时间）java

---
+++ app/src/main/java/com/example/finalprojectapplication/PaymentRecord.java
@@ -0,0 +1,40 @@
+package com.example.finalprojectapplication;
+
+public class PaymentRecord {
+    private int id;
+    private String account;
+    private double amount;
+    private String date;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
